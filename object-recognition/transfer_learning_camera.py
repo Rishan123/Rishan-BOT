23640b85782f4c4e8366c79289feaee36c50e3cf
Rishan123 <alok76in@gmail.com>
2020-10-02 12:08:07 +0100

    object recognition with picamera

--- object-recognition/transfer_learning_camera.py
+++ object-recognition/transfer_learning_camera.py
@@ -1,22 +1,16 @@
 import numpy as np
 import tensorflow as tf
 import tensorflow_hub as hub
-import PIL.Image as Image
-from picamera import PiCamera
-
-camera = PiCamera()
-camera.resolution = (224, 224)
 
 classifier_model ="https://tfhub.dev/google/tf2-preview/mobilenet_v2/classification/4"
 IMAGE_SHAPE = (224, 224)
 
-camera.capture('image.jpg')
 classifier = tf.keras.Sequential([
     hub.KerasLayer(classifier_model, input_shape=IMAGE_SHAPE+(3,))
 ])
 
-# sample_img = tf.keras.utils.get_file('image.jpg','https://storage.googleapis.com/download.tensorflow.org/example_images/grace_hopper.jpg')
-sample_img = Image.open('image.jpg').resize(IMAGE_SHAPE)
+sample_img = tf.keras.utils.get_file('image.jpg','https://storage.googleapis.com/download.tensorflow.org/example_images/grace_hopper.jpg')
+sample_img = Image.open(sample_img).resize(IMAGE_SHAPE)
 sample_img = np.array(sample_img)/255.0
 
 result = classifier.predict(sample_img[np.newaxis, ...])
