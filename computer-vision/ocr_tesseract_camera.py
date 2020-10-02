5402d2f22efeaf3637a106d95ec928ee0333c441
Rishan123 <alok76in@gmail.com>
2020-10-02 14:44:48 +0100

    modules are nearly complete, next add motors

--- computer-vision/ocr_tesseract_camera.py
+++ computer-vision/ocr_tesseract_camera.py
@@ -33,31 +33,3 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     if key == ord("q"):
         break
 
-
-
-camera = PiCamera()
-camera.resolution = (224, 224)
-image_path = 'image.jpg'
-camera.capture(image_path)
-# load the example image and convert it to grayscale
-image = cv2.imread(image_path)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-gray = cv2.threshold(gray, 0, 255,
-    cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-
-# gray = cv2.medianBlur(gray, 3)
-
-# write the grayscale image to disk as a temporary file so we can
-# apply OCR to it
-filename = "{}.png".format(os.getpid())
-cv2.imwrite(filename, gray)
-
-# load the image as a PIL/Pillow image, apply OCR, and then delete
-# the temporary file
-text = pytesseract.image_to_string(Image.open(filename))
-os.remove(filename)
-print(text)
-# show the output images
-cv2.imshow("Image", image)
-cv2.imshow("Output", gray)
-cv2.waitKey(0)
--- computer-vision/ocr_tesseract_text-to-speech.py
+++ computer-vision/ocr_tesseract_text-to-speech.py
@@ -4,8 +4,9 @@ import cv2
 from gtts import gTTS
 import os
 
+chars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0']
 # load the example image and convert it to grayscale
-image = cv2.imread('/home/pi/Rishan-BOT/computer-vision/example_01.png')
+image = cv2.imread('/home/pi/Rishan-BOT/computer-vision/example_03.png')
 config = ('-l eng --oem 1 --psm 3')
 
 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -13,9 +14,11 @@ image = cv2.threshold(image, 0, 255,
     cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 
 text = pytesseract.image_to_string(image, config=config)
-tts = gTTS(text)
-tts.save('/home/pi/text.mp3')
-os.system('omxplayer /home/pi/text.mp3')
+for i in text:
+    if i is not None and i in chars:
+        tts = gTTS(text)
+        tts.save('/home/pi/text.mp3')
+        os.system('omxplayer /home/pi/text.mp3')
 
 print(text)
 cv2.imshow('OCR',image)
