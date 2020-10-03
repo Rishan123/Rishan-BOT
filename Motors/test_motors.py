31523a7893222aecce31fde7c74a207cfa37148d
Rishan123 <alok76in@gmail.com>
2020-10-03 16:16:22 +0100

    changed motors

--- Motors/test_motors.py
+++ Motors/test_motors.py
@@ -2,7 +2,14 @@ from gpiozero import Robot
 from time import sleep
 
 robot = Robot(left=(18,3), right=(15,14))
+speed = 0.4
 
-robot.forward(speed=0.4)
+robot.forward(speed=speed)
+sleep(1)
+robot.backward(speed=speed)
+sleep(1)
+robot.left()
+sleep(1)
+robot.right()
 sleep(1)
 robot.stop()
